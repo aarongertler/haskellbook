dfa71e1b5a15c890a379f078f4364ba0ebea1723
Aaron Gertler <aaronlgertler@gmail.com>
2018-06-27 23:30:35 -0700

    learned more about list functions, quick session

--- greatgood/start.hs
+++ greatgood/start.hs
@@ -31,3 +31,39 @@ smallCat = 'A':" SMALL CAT" -- "A SMALL CAT"
 -- Adding to the end of a list requires another list, but regular numbers and strings can be added to the front of a list
 
 -- [1,2,3] is syntactic sugar for 1:2:3:[]   (interesting!)
+
+extract = "String extraction" !! 7 -- "e", returns indexed character
+
+-- Lists can only contain instances of one data type
+
+-- head [1,2,3,4] -- 1
+-- tail [1,2,3,4] -- [2,3,4]
+-- last [1,2,3,4] -- 4
+-- init [1,2,3,4] -- [1,2,3]
+-- length [1,2,3,4] -- 4
+-- null [] -- true
+-- reverse [1,2,3,4] -- [4,3,2,1]
+-- take 2 [1,2,3,4] -- [1,2]
+-- take 5 [1,2,3,4] -- [1,2,3,4]
+-- drop 2 [1,2,3,4] -- [3,4] (Removes first two elements)
+-- drop 5 [1,2,3,4] -- []
+-- minimum [1,2,3,4] -- 1
+-- maximum [1,2,3,4] -- 4
+-- sum [1,2,3,4] -- 10
+-- product [1,2,3,4] -- 24
+-- 3 `elem` [1,2,3,4] -- True
+a = [1..4] -- [1,2,3,4]
+b = ['g'..'j'] -- ['g','h','i','j']
+c = [2,5..14] -- [2,5,8,11,14]
+d = [2,5..16] -- [2,5,8,11,14] -> doesn't matter if our last number doesn't fit our "step"
+e = [20,19..1] -- Reverse list from 20 to 1, you do need that 19
+
+-- Don't use floats in list ranges, that stuff gets weird
+
+f = take 24 [13,26..] -- Returns first 24 multiples of 13!
+
+g = take 10 (cycle [1,2,3,4]) -- [1,2,3,4,1,2,3,4,1,2]
+
+h = take 5 (repeat 1) -- [1,1,1,1,1]
+replicate 1 5 -- [1,1,1,1,1]
+ 
