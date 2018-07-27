39b964c2905368034b520ca493afc7bbd035b061
Aaron Gertler <aaronlgertler@gmail.com>
2018-07-26 23:15:30 -0700

    From input/output to files and streams

---
+++ greatgood/files.hs
@@ -0,0 +1,1 @@
+-- Files and streams in Haskell
--- greatgood/input_output.hs
+++ greatgood/input_output.hs
@@ -25,7 +25,7 @@ import Data.Char
 main = do
 	line <- getLine
 	if null line
-		then return ()
+		then return () -- an "empty" I/O action, not an end to the program (if it weren't in an if/then, we could keep going after the return)
 		else do 
 			putStrLn (reverseWords line)
 			main -- Now we have an input loop! We'll keep asking for words until the user hits "enter" with no word, at which point we return nothing
@@ -45,3 +45,52 @@ reverseWords = unwords . map reverse . words -- "Words" turns a string into a sp
 -- Definition of composition: (f . g) x = f (g x)
 -- So the above broken code is trying to return a function call rather than a string? That's my best interpretation... #QUESTION
 -- While the correct code knows it will be running all the functions on a string, and thus will get the right output? ...yeah, #QUESTION
+
+
+-- OTHER NOTES ON INPUT/OUTPUT:
+
+-- putStr is like putStrLn without a line break (think of Ruby, puts vs. put)
+-- putChar outputs one character at a time (and is recurred to build putStr)
+-- print = putStrLn . show  (turns anything that can be a string into a string with "show", then prints it) (ghci uses this for terminal work!)
+
+
+-- Every program that takes user input will only operate once we hit "enter" (even if we tell it to stop accepting new data after we enter a space --
+	-- if that happens, we'll just get the first word of whatever we wrote)
+
+
+-- "when" is Haskell's way of saying "as long as..." 
+-- (replaces "if something do I/O else return ()" with "when something do I/O", handles the "return on False" automatically)
+
+
+-- "sequence" lets you line up a list of I/O actions:
+
+-- main = do  
+--     a <- getLine  
+--     b <- getLine  
+--     c <- getLine  
+--     print [a,b,c]  
+
+-- ...is the same as:
+
+-- main = do  
+--     rs <- sequence [getLine, getLine, getLine]  
+--     print rs  
+
+
+-- use mapM_ when you want to print a series of items from a mapped list, but don't want a list of empty returns to print out at the end
+
+
+-- main = forever $ do (stuff)   -- this will cause some console thing to happen forever, with no return condition (is this a common occurrence?)
+
+
+-- forM = "make an I/O action for every element in this list" 
+-- (example: Printing a set of quiz questions, saving each answer, then feeding all answers back to the user) (see book example)
+-- Helps with readability, doesn't do anything you couldn't replace with other functions
+
+
+
+-- Author's guide to thinking through this stuff:
+
+-- Don't think of a function like putStrLn as a function that takes a string and prints it to the screen. 
+-- Think of it as a function that takes a string and returns an I/O action. 
+-- That I/O action will, when performed, print beautiful poetry to your terminal.
