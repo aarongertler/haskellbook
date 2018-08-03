20a049df6f75385163a9f66269c3e5f7c4343e83
Aaron Gertler <aaronlgertler@gmail.com>
2018-08-02 23:54:12 -0700

    Still thinking through reverse Polish notation

--- greatgood/fsp_1.hs
+++ greatgood/fsp_1.hs
@@ -1,3 +1,14 @@
 -- Functionally Solving Problems, problem 1
 
 -- (Splitting up these files so that I can actually make the code compile for once)
+
+-- Reverse Polish Notation solution is interesting, but I'm a little down on the mathematical system...
+
+-- import Data.List
+
+solveRPN :: (Num a) => String -> a
+solveRPN = head . foldl pFold [] . string 
+    where  pFold (x:y:ys) "*" = (x * y):ys
+           pFold (x:y:ys) "+" = (x + y):ys
+           pFold (x:y:ys) "-" = (x - y):ys
+           pFold ys numString = read numString:ys -- if we hit a number, turn it into a Num and add it to the stack
